be7c05ccfa67f72c5f1e6181eb0ff9c7cf744b23
lesya_popova <lspopova99@gmail.com>
2023-06-16 10:16:21 +0800

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿void WeekDay(int day)
+{
+    if (day < 1 || day > 7) Console.Write("There are only seven days in a week!");
+    if (day == 6 || day == 7) Console.Write("It is a weekend! Have fun!");
+    if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5)
+    {
+        Console.Write("It is a weekday, wake up and go to work!");
+    }
+}
+Console.Write("Enter the number of the day: ");
+int number = Convert.ToInt32(Console.ReadLine());
+WeekDay(number);
