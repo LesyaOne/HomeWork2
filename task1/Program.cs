7182e909467cbd00b1a33ea351c48b86863893e9
lesya_popova <lspopova99@gmail.com>
2023-06-16 09:28:04 +0800

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int SecondDigit(int num)
+{
+    int result = (num % 100 - (num %10)) / 10;
+    if (result < 0) result = result * (-1);
+    return result;
+}
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number>99 && number<1000) || (number< -99 && number > -1000))
+{
+    Console.WriteLine($"Second digit is {SecondDigit(number)}");
+}
+else Console.WriteLine("Wrong number, try again");
